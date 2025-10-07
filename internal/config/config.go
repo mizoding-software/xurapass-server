4fcd001e614966d781d9ae160d4250101626a0e8
eliuttth-dev <eliuttth@gmail.com>
2025-10-07 23:30:44 +0200

    [SP-1.2] - Configure connections

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -25,8 +26,8 @@ type Config struct {
 
 	JWTSecret	string
 	JWTExp		time.Duration
-	EncryptKey	string
-	BCryptCost	string
+	EncryptionKey	string
+	BCryptCost	int
 
 	RateLimitRequest	int
 	RateLimitWindow		time.Duration
@@ -40,12 +41,55 @@ func Load() (*Config, error) {
 		Environment: getEnv("ENVIRONMENT", "development"),
 		ServerPort: getEnv("SERVER_PORT", "8080"),
 		LogLevel: getEnv("LOG_LEVEL", "info"),
-		
+
+		DBHost: getEnv("DB_HOST", "localhost"),
+		DBPort: getEnv("DB_PORT", "5432"),
+		DBUser: getEnv("DB_USER", "xurapass"),
+		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBName: getEnv("DB_NAME", "xurapass"),
+		DBSSLMode: getEnv("DB_SSL_MODE", "disable"),
+
+		RedisURL: getEnv("REDIS_URL", "redis://localhost:6379"),
+
+		JWTSecret: getEnv("JWT_SECRET", ""),
+		JWTExp: getEnvAsDuration("JWT_EXPIRATION", 24 * time.Hour),
+
+		EncryptionKey: getEnv("ENCRYPTION_KEY", ""),
+		BCryptCost: getEnvAsInt("BCRYPT_COST", 12),
+
+		RateLimitRequest: getEnvAsInt("RATE_LIMIT_REQUESTS",100),
+		RateLimitWindow: getEnvAsDuration("RATE_LIMIT_WINDOW", 1*time.Minute),
 	}
+
+	if cfg.JWTSecret == "" {
+		panic("JWT_SECRET is required")
+	}
+
+	if cfg.EncryptionKey == "" {
+		panic("ENCRYPTION_KEY is required")
+	}
+
+	return cfg, nil
 }
 
 func getEnv(key, defaultValue string) string {
 	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvAsInt(key string, defaultValue int) int {
+	valueStr := getEnv(key, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvAsDuration(key string, defaultValue time.Duration) time.Duration {
+	valueStr := getEnv(key, "")
+	if value, err := time.ParseDuration(valueStr); err == nil {
 		return value
 	}
 	return defaultValue
