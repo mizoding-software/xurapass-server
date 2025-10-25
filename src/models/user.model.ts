62ee07580bda5d151726ad828651f917576dfd96
eliuttth-dev <eliuttth@gmail.com>
2025-10-25 13:50:42 +0200

    [XUR-2] Add User Model Functions

---
+++ src/models/user.model.ts
@@ -0,0 +1,56 @@
+export const createNewUser = () => {};
+
+// Search
+export const searchUser = () => {};
+export const getUserById = () => {};
+export const getUserByUsername = () => {};
+export const getUserByEmail = () => {};
+
+// User Management
+export const updateUserProfile = () => {};
+export const deactivateUser = () => {};
+export const deleteUser = () => {};
+export const changeEmail = () => {};
+export const changeUsername = () => {};
+
+// Password Management
+export const recoverPassword = () => {};
+export const changePassword = () => {};
+export const validatePasswordResetToken = () => {};
+export const expirePasswordResetTokens = () => {};
+export const forcePasswordChange = () => {};
+
+// Auth & Session Management
+export const validatePassword = () => {};
+export const createSession = () => {};
+export const invalidSession = () => {};
+export const refreshToken = () => {};
+export const verifyToken = () => {};
+
+// Account Verification
+export const verifyEmail = () => {};
+export const sendVerificationEmail = () => {};
+export const verifyPhone = () => {};
+
+// Security Features
+export const lockAccount = () => {};
+export const unlockAccount = () => {};
+export const checkPasswordStrength = () => {};
+export const logSecurityEvent = () => {};
+export const checkSuspiciousActivity = () => {};
+
+// Multi-Factor Auth
+export const enableMFA = () => {};
+export const disableMFA = () => {};
+export const verifyMFA = () => {};
+export const generateMFABackupCodes = () => {};
+
+// Admin User Management
+export const listUsers = () => {};
+export const getUserSessions = () => {};
+export const getUserStats = () => {};
+
+// Security & Compliance
+export const exportUserData = () => {};
+export const anonymizeUserData = () => {};
+export const checkCompliance = () => {};
